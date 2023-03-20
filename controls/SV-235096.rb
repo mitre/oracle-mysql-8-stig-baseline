2e0c55a34d09961ff2b7aec82baa69035032daff
Eugene Aronne <34140975+ejaronne@users.noreply.github.com>
2023-03-20 15:28:13 -0400

    Update SV-235096.rb

--- controls/SV-235096.rb
+++ controls/SV-235096.rb
@@ -112,7 +112,7 @@ given period of time.
      mysql.user 
   WHERE
      user not like 'mysql.%' 
-     and user not in (mysql_administrative_users);
+     and user like 'root';
   )
 
   describe "Global value of max_user_connections" do
