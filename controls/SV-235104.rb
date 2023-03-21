98d87647cafdd2143e8ba3510ec19002da623dd6
Shivani Karikar <karikarshivani@gmail.com>
2023-03-21 12:39:41 -0400

    Updated with rds_admin value concat and changed matcher
    Signed-off-by: Shivani Karikar <karikarshivani@gmail.com>

--- controls/SV-235104.rb
+++ controls/SV-235104.rb
@@ -57,12 +57,11 @@ personnel to select which auditable events are audited.
 
   sql_session = mysql_session(input('user'), input('password'), input('host'), input('port'))
 
-  # Test then implement for all other user inputs
   if !input('aws_rds')
     audit_admins = input('audit_admins')
   else
     audit_admins = input('audit_admins').concat(["'rdsadmin'@'localhost'"])
-
+  end
 
   query_audit_admins = %(
     SELECT
@@ -79,6 +78,6 @@ personnel to select which auditable events are audited.
 
   describe "List of configured audit admins" do
     subject { sql_session.query(query_audit_admins).results.column('grantee') }
-    it { should match_array audit_admins }
+    it { should be_in audit_admins }
   end
 end
