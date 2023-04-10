d260d1786f1db0e4bbdafde78a9703e982f02a88
wajidraja <126132622+wajidraja@users.noreply.github.com>
2023-04-10 19:19:25 +0000

    updated sv-235190
    Signed-off-by: wajidraja <126132622+wajidraja@users.noreply.github.com>

--- controls/SV-235190.rb
+++ controls/SV-235190.rb
@@ -67,8 +67,14 @@ exact behavior of FIPS mode for ON or STRICT depends on the OpenSSL version.
 
   ssl_params = sql_session.query(query_ssl_params).results
 
+
+describe.one do
   describe '@@ssl_fips_mode' do
     subject { ssl_params.column('@@ssl_fips_mode').join }
-    it { should match /1|ON/ }
+    it { should cmp 'ON' }
+  end
+  describe '@@ssl_fips_mode' do
+    subject { ssl_params.column('@@ssl_fips_mode').join }
+    it { should cmp '1' }
   end
 end
