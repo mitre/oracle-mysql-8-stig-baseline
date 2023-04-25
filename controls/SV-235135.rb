9320e109b7aefee53c775b61125806c237faafb7
Shivani Karikar <karikarshivani@gmail.com>
2023-04-25 15:03:39 -0400

    Removed subject and declared a variable instead
    Signed-off-by: Shivani Karikar <karikarshivani@gmail.com>

--- controls/SV-235135.rb
+++ controls/SV-235135.rb
@@ -160,10 +160,10 @@ STRICT at startup causes the server to produce an error message and exit.
 
   ssl_params = sql_session.query(query_ssl_params).results
 
+  ssl_fips_mode = ssl_params.column('@@ssl_fips_mode').join
   describe '@@ssl_fips_mode' do
-    subject { ssl_params.column('@@ssl_fips_mode').join }
-    failure_message = "@@ssl_fips_mode should be set to ON or STRICT and not #{subject}"
-    expect(subject).to  be_in(['ON', 'STRICT']), failure_message
+    failure_message = "@@ssl_fips_mode should be set to ON or STRICT and not #{ssl_fips_mode}"
+    expect(ssl_fips_mode).to  be_in(['ON', 'STRICT']), failure_message
   end
 
   describe.one do
