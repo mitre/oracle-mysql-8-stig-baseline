d7c3f43503ce9761d290cff6010757fa74db8cec
Shivani Karikar <karikarshivani@gmail.com>
2023-05-01 11:47:30 -0400

    Corrected value in it statement
    Signed-off-by: Shivani Karikar <karikarshivani@gmail.com>

--- controls/SV-235134.rb
+++ controls/SV-235134.rb
@@ -155,7 +155,7 @@ names as necessary:
 
   require_secure_transport = ssl_params.column('@@require_secure_transport').join
   describe '@@require_secure_transport' do
-    it "shoud be ON or STRICT. Got #{require_secure_transport}" do
+    it "shoud be ON or 1. Got #{require_secure_transport}" do
       expect(require_secure_transport).to be_in(['ON', '1'])
     end
   end
