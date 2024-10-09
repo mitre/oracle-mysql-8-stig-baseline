50ec175206a1e093546a46e147dfa4130c688e7e
Eugene Aronne <34140975+ejaronne@users.noreply.github.com>
2024-10-09 11:17:11 -0400

    Update SV-235194.rb

--- controls/SV-235194.rb
+++ controls/SV-235194.rb
@@ -77,7 +77,7 @@ patches are consistently applied to MySQL within the time allowed."
 
   version = sql_session.query(query_version).results
 
-  describe 'The installed mysql version: #{version}' do
+  describe "The installed mysql version: #{version}" do
     subject { version.column('@@version').join }
     it { should cmp >= minimum_mysql_version }
   end
